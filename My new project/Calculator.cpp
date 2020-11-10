c26580cfa5c34cb06bd66d2f47df046590b705d8
Nikolay <kolev_83@yahoo.co.uk>
2020-11-10 10:35:32 +0000

    calculator with function

--- My new project/Calculator.cpp
+++ My new project/Calculator.cpp
@@ -2,13 +2,134 @@
 #include <string>
 #include <math.h>
 
-int main()
+
+
+void Add()
 {
+
 	float number1 = 0;
 	float number2 = 0;
 	float result = 0.0f;
-	char opera = ' ';
 
+
+
+	std::cout << " Please enter number 1 : " << std::endl;
+	std::cin >> number1;
+	std::cout << " Please enter number 2 : " << std::endl;
+	std::cin >> number2;
+	result = number1 + number2;
+	std::cout << " The result is : " << result << std::endl;
+
+}
+
+void Subtraction()
+{
+
+	float number1 = 0;
+	float number2 = 0;
+	float result = 0.0f;
+
+
+
+	std::cout << " Please enter number 1 : " << std::endl;
+	std::cin >> number1;
+	std::cout << " Please enter number 2 : " << std::endl;
+	std::cin >> number2;
+	result = number1 - number2;
+	std::cout << " The result is : " << result << std::endl;
+
+}
+
+void Division()
+{
+
+	float number1 = 0;
+	float number2 = 0;
+	float result = 0.0f;
+
+
+
+	std::cout << " Please enter number 1 : " << std::endl;
+	std::cin >> number1;
+	std::cout << " Please enter number 2 : " << std::endl;
+	std::cin >> number2;
+	result = number1 / number2;
+	std::cout << " The result is : " << result << std::endl;
+
+}
+
+void Multiplication()
+{
+
+	float number1 = 0;
+	float number2 = 0;
+	float result = 0.0f;
+
+
+
+	std::cout << " Please enter number 1 : " << std::endl;
+	std::cin >> number1;
+	std::cout << " Please enter number 2 : " << std::endl;
+	std::cin >> number2;
+	result = number1 * number2;
+	std::cout << " The result is : " << result << std::endl;
+
+}
+
+void Power()
+{
+
+	float number1 = 0;
+	float number2 = 0;
+	float result = 0.0f;
+
+
+
+	std::cout << " Please enter number 1 : " << std::endl;
+	std::cin >> number1;
+	std::cout << " Please enter number 2 : " << std::endl;
+	std::cin >> number2;
+	float result2 = number1;
+	int i = 1;
+	while (i < number2)
+	{
+		result2 = result2 * number1;
+		i++;
+
+	}
+
+	std::cout << " The result is : " << result2 << std::endl;
+
+}
+
+void Reminder()
+{
+
+	float number1 = 0;
+	float number2 = 0;
+	float quotient;
+	float reminder;
+
+
+	std::cout << " Please enter number 1 : " << std::endl;
+	std::cin >> number1;
+	std::cout << " Please enter number 2 : " << std::endl;
+	std::cin >> number2;
+
+
+	quotient = number1 / number2;
+
+
+
+	std::cout << " Quotient is : " << quotient << std::endl;
+
+}
+
+
+int main()
+{
+
+	char opera = ' ';
 
 	std::cout << "	         _   _      " << std::endl;
 	std::cout << "                | | | |      " << std::endl;
@@ -24,69 +145,50 @@ int main()
 	std::cout << " |Press 3. For division        |" << std::endl;
 	std::cout << " |Press 4. For multiplication  |" << std::endl;
 	std::cout << " |Press 5. For power of        |" << std::endl;
-	std::cout << " |Press 6. Forreminder         |" << std::endl;
+	std::cout << " |Press 6. For reminder        |" << std::endl;
 	std::cout << " |#############################|" << std::endl;
+
 
 
 	std::cout << " Please enter number from the menu : " << std::endl;
 	std::cin >> opera;
-	std::cout << " Please enter number 1 : " << std::endl;
-	std::cin >> number1;
-	std::cout << " Please enter number 2 : " << std::endl;
-	std::cin >> number2;
+
+
 
 
 
 	if (opera == '1')
 	{
-		result = number1 + number2;
-		std::cout << " The result is : " << result << std::endl;
+		Add();
+
 	}
 	else if (opera == '2')
 	{
 
-		result = number1 - number2;
-		std::cout << " The result is : " << result << std::endl;
+		Subtraction();
 	}
 	else if (opera == '3')
 	{
 
-		result = number1 / number2;
-		std::cout << " The result is : " << result << std::endl;
+		Division();
+
 	}
 	else if (opera == '4')
 	{
+		Multiplication();
 
-		result = number1 * number2;
-		std::cout << " The result is : " << result << std::endl;
 	}
 	else if (opera == '5')
 	{
 
-		float result2 = number1;
-		int i = 1;
-		while (i < number2)
-		{
-			result2 = result2 * number1;
-			i++;
+		Power();
 
-		}
-		
-		std::cout << " The result is : " << result2 << std::endl;
-		
 	}
 	else if (opera == '6')
 	{
-		
-		float quotient;
-		float reminder;
 
-		quotient = number1 / number2;
+		Reminder();
 
-		
-
-		std::cout << " Quotient is : " << quotient << std::endl;
-	
 
 	}
 
