9d239d4a7b32b7b3e89f6a3c9925f57f6707e87f
Nikolay <kolev_83@yahoo.co.uk>
2020-11-23 23:44:31 +0000

    menu and choice in

--- ConsoleGameCU5011/ConsoleGameCU5011/ConsoleGameCU5011.cpp
+++ ConsoleGameCU5011/ConsoleGameCU5011/ConsoleGameCU5011.cpp
@@ -7,7 +7,7 @@ int main()
     // game intro
 
     
-    std::cout << "  ____                                      "<< std::endl;                                     
+   
     std::cout << " /\  _\                                     "<< std::endl;             
     std::cout << " \ \ \ _   __      ___     ___   __  __     "<< std::endl;          
     std::cout << "  \ \  _\/'__`\  /' _ `\  /'___\/\ \/\ \    "<< std::endl;         
@@ -47,14 +47,30 @@ int main()
 
     // character input 
     std::string name = " ";
-    std::string gameName = " ";
+    std::string yourLocation = " ";
 
     std::cout << "Please enter your name : ";
     std::cin >> name;
     std::cout << "Please enter the name of the game which you want to play : ";
-    std::cin >> gameName;
-    std::cout <<"Hi " <<name <<". Your game of " << gameName << " will start now !" ;
+    std::cin >> yourLocation;
+    std::cout <<"Hi " <<name <<" from " << yourLocation << std::endl;
+    std::cout << ". Your game will start now ! " << std::endl;
+    std::cout << " Please choice number from the menu :" << std::endl;
 
-   
+
+    // menu
+    int choice;
+
+
+
+            std::cout << "##################################" << std::endl;
+            std::cout << "             MENU                 " << std::endl;
+            std::cout << "##################################" << std::endl;
+            std::cout << "1.      GO TO NEXT GAME           " << std::endl;
+            std::cout << "2.     STAY ON THAT GAME          " << std::endl;
+            std::cout << "3.  RETURN TO THE PREVIOUS GAME   " << std::endl;
+            std::cout << "4.           EXIT                 " << std::endl;
+            std::cout << "##################################" << std::endl;
+          
     
 }
