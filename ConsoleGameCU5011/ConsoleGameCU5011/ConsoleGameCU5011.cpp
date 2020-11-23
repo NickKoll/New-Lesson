91aa490ca336cd0991ef5754802afb3df19ca56d
Nikolay <kolev_83@yahoo.co.uk>
2020-11-23 23:57:14 +0000

    menu function

--- ConsoleGameCU5011/ConsoleGameCU5011/ConsoleGameCU5011.cpp
+++ ConsoleGameCU5011/ConsoleGameCU5011/ConsoleGameCU5011.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
 #include <windows.h>
 
+void Menu()
+{
+    int choice;
 
+
+
+    std::cout << "##################################" << std::endl;
+    std::cout << "             MENU                 " << std::endl;
+    std::cout << "##################################" << std::endl;
+    std::cout << "1.      GO TO NEXT GAME           " << std::endl;
+    std::cout << "2.     STAY ON THAT GAME          " << std::endl;
+    std::cout << "3.  RETURN TO THE PREVIOUS GAME   " << std::endl;
+    std::cout << "4.           EXIT                 " << std::endl;
+    std::cout << "##################################" << std::endl;
+
+    std::cin >> choice;
+
+    if (choice == 1)
+    {
+        std::cout << "1" << std::endl;
+    }
+    else if (choice == 2)
+    {
+        std::cout << "2." << std::endl;
+    }
+    else if (choice == 3)
+    {
+        std::cout << "3." << std::endl;
+    }
+    else if (choice == 4)
+    {
+        exit;
+    }
+    else
+    {
+        std::cout << "Invalid number . Please enter 1, 2, 3 or 4." << std::endl;
+        Menu();
+    }
+
+
+}
 int main()
 {
     // game intro
@@ -19,7 +59,7 @@ int main()
     
     Sleep(400);
 
-    std::cout << "                                 "<< " ____       : " << std::endl;
+    
     std::cout << "                                 "<<" /\  _\                             : "               << std::endl;  
     std::cout << "                                 "<<" \ \ \ _   __      ___     ___   __  __: "            << std::endl;  
     std::cout << "                                 "<<"  \ \  _\/'__`\  /' _ `\  /'___\/\ \/\ \: "           << std::endl;  
@@ -31,7 +71,7 @@ int main()
     
     Sleep(400);
 
-    std::cout << "                                                                    " << " ____                                       : "   << std::endl;  
+                                     
     std::cout << "                                                                    " << " /\  _\                                     : "   << std::endl;  
     std::cout << "                                                                    " << " \ \ \ _   __      ___     ___   __  __     : "   << std::endl;  
     std::cout << "                                                                    " << "  \ \  _\/'__`\  /' _ `\  /'___\/\ \/\ \    : "   << std::endl;  
@@ -51,26 +91,15 @@ int main()
 
     std::cout << "Please enter your name : ";
     std::cin >> name;
-    std::cout << "Please enter the name of the game which you want to play : ";
+    std::cout << "Please enter the name of your city : ";
     std::cin >> yourLocation;
     std::cout <<"Hi " <<name <<" from " << yourLocation << std::endl;
     std::cout << ". Your game will start now ! " << std::endl;
     std::cout << " Please choice number from the menu :" << std::endl;
 
 
-    // menu
-    int choice;
-
-
-
-            std::cout << "##################################" << std::endl;
-            std::cout << "             MENU                 " << std::endl;
-            std::cout << "##################################" << std::endl;
-            std::cout << "1.      GO TO NEXT GAME           " << std::endl;
-            std::cout << "2.     STAY ON THAT GAME          " << std::endl;
-            std::cout << "3.  RETURN TO THE PREVIOUS GAME   " << std::endl;
-            std::cout << "4.           EXIT                 " << std::endl;
-            std::cout << "##################################" << std::endl;
+    Menu();
+   
           
-    
+    return 0;
 }
