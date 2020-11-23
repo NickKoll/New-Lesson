4bcf8327df91cb3f8c1b49a632ad20f9f5aea6f9
Nikolay <kolev_83@yahoo.co.uk>
2020-11-23 21:58:00 +0000

    Intro

--- ConsoleGameCU5011/ConsoleGameCU5011/ConsoleGameCU5011.cpp
+++ ConsoleGameCU5011/ConsoleGameCU5011/ConsoleGameCU5011.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 
+
+
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+    std::string name = " ";
+    std::string gameName = " ";
 
+    std::cout << "Please enter your name : ";
+    std::cin >> name;
+    std::cout << "Please enter the name of the game which you want to play : ";
+    std::cin >> gameName;
+    std::cout <<"Hi " <<name <<". Your game of " << gameName << "will start now !" ;
+
+   
+    
+}
