80fd4f3f60d6cea561b64f606968f3d2bd2fcd36
Nikolay <kolev_83@yahoo.co.uk>
2020-11-23 22:45:09 +0000

    game intro

--- ConsoleGameCU5011/ConsoleGameCU5011/ConsoleGameCU5011.cpp
+++ ConsoleGameCU5011/ConsoleGameCU5011/ConsoleGameCU5011.cpp
@@ -1,9 +1,51 @@
 #include <iostream>
-
+#include <windows.h>
 
 
 int main()
 {
+    // game intro
+
+    
+    std::cout << "  ____                                      "<< std::endl;                                     
+    std::cout << " /\  _\                                     "<< std::endl;             
+    std::cout << " \ \ \ _   __      ___     ___   __  __     "<< std::endl;          
+    std::cout << "  \ \  _\/'__`\  /' _ `\  /'___\/\ \/\ \    "<< std::endl;         
+    std::cout << "   \ \ \/\ \L\.\_/\ \/\ \/\ \__   \ \_\ \   "<< std::endl;        
+    std::cout << "    \ \_\ \__/.\_\ \_\ \_\ \____\\/`____ \  "<< std::endl;       
+    std::cout << "     \/_/\/__/\/_/\/_/\/_/\/____/ `/___/> \ "<< std::endl;      
+    std::cout << "                                     /\___/ "<< std::endl;      
+    std::cout << "                                     \/__/  "<< std::endl;      
+    
+    Sleep(400);
+
+    std::cout << "                                 "<< " ____       : " << std::endl;
+    std::cout << "                                 "<<" /\  _\                             : "               << std::endl;  
+    std::cout << "                                 "<<" \ \ \ _   __      ___     ___   __  __: "            << std::endl;  
+    std::cout << "                                 "<<"  \ \  _\/'__`\  /' _ `\  /'___\/\ \/\ \: "           << std::endl;  
+    std::cout << "                                 "<<"   \ \ \/\ \L\.\_/\ \/\ \/\ \__   \ \_\ \: "          << std::endl;  
+    std::cout << "                                 "<<"    \ \_\ \__/.\_\ \_\ \_\ \____\\/`____ \: "         << std::endl;  
+    std::cout << "                                 "<<"     \/_/\/__/\/_/\/_/\/_/\/____/ `/___/> \: "        << std::endl;  
+    std::cout << "                                 "<<"                                     /\___/: "        << std::endl;  
+    std::cout << "                                 "<<"                                     \/__/ : "        << std::endl;  
+    
+    Sleep(400);
+
+    std::cout << "                                                                    " << " ____                                       : "   << std::endl;  
+    std::cout << "                                                                    " << " /\  _\                                     : "   << std::endl;  
+    std::cout << "                                                                    " << " \ \ \ _   __      ___     ___   __  __     : "   << std::endl;  
+    std::cout << "                                                                    " << "  \ \  _\/'__`\  /' _ `\  /'___\/\ \/\ \    : "   << std::endl;  
+    std::cout << "                                                                    " << "   \ \ \/\ \L\.\_/\ \/\ \/\ \__   \ \_\ \   : "   << std::endl;  
+    std::cout << "                                                                    " << "    \ \_\ \__/.\_\ \_\ \_\ \____\\/`____ \  : "   << std::endl;  
+    std::cout << "                                                                    " << "     \/_/\/__/\/_/\/_/\/_/\/____/ `/___/> \ : "   << std::endl;  
+    std::cout << "                                                                    " << "                                     /\___/ : "   << std::endl;  
+    std::cout << "                                                                    " << "                                     \/__/  : "   << std::endl;  
+
+    Sleep(400);
+
+
+
+    // character input 
     std::string name = " ";
     std::string gameName = " ";
 
@@ -11,7 +53,7 @@ int main()
     std::cin >> name;
     std::cout << "Please enter the name of the game which you want to play : ";
     std::cin >> gameName;
-    std::cout <<"Hi " <<name <<". Your game of " << gameName << "will start now !" ;
+    std::cout <<"Hi " <<name <<". Your game of " << gameName << " will start now !" ;
 
    
     
