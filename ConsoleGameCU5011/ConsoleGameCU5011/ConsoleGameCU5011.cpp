60ba32184396d4ca21fab7122f62241a00efaef3
Nikolay <kolev_83@yahoo.co.uk>
2020-12-01 00:32:04 +0000

    menu into enum

--- ConsoleGameCU5011/ConsoleGameCU5011/ConsoleGameCU5011.cpp
+++ ConsoleGameCU5011/ConsoleGameCU5011/ConsoleGameCU5011.cpp
@@ -1,26 +1,11 @@
 #include <iostream>
-
+//menu choice
+enum  MenuCoice {NEXT_CASINO = 1, CURENT_CASINO, PREVIOUS_CASINO, EXIT};
 //rooms in the casino
 enum class CasinoRoom {ROOM_1, ROOM_2, ROOM_3};
 //diferent casinos for the player to choose
 enum class CasinoBuilding {ROYAL, VEGAS, MONACO};
-//Global variables================================
-std::string name;
 
-//current player funds and maximum player funds 
-int playerFunds = 0;
-int playerFundsMax = 0;
-
-//money earn from work and stored in the bank
-int bank = 150;
-
-//money which can be earn from the casino
-int casinoFunds = 0;
-int choice;
-//levels in the specific casino
-CasinoBuilding currentCasino = CasinoBuilding::ROYAL;
-CasinoBuilding previousCasino = CasinoBuilding::ROYAL;
-CasinoRoom casinoRoom = CasinoRoom::ROOM_1;
 
 
 //================================================
@@ -42,7 +27,12 @@ int Menu()
     std::cout << "4.             EXIT               " << std::endl;
     std::cout << "##################################" << std::endl;
 
-    std::cin >> choice;
+    do
+    {
+        std::cout << " Please make your choice . " << std::endl;
+        std::cin >> choice;
+    } while (choice > MenuCoice::EXIT || choice < MenuCoice::NEXT_CASINO);
+
     return choice;
 
 }
@@ -52,16 +42,60 @@ void Info()
 
 }
 //================================================
-    void Casino()
+void Casino()
+{
+   std::cout << " Welcome to the casino " << std::endl;
+  
+}
+//================================================
+//display casino name
+void DisplayCasinoName(CasinoBuilding currentCasino)
+{
+
+    switch (currentCasino)
     {
-        std::cout << " Welcome to the casino " << std::endl;
-        
+        case CasinoBuilding::ROYAL:
+        {
+            std::cout << " Royal Building " << std::endl;
+            break;
+        }
+
+        case CasinoBuilding::VEGAS:
+        {
+            std::cout << " Vegas Building " << std::endl;
+            break;
+        }
+
+        case CasinoBuilding::MONACO:
+        {
+            std::cout << " Monaco Building " << std::endl;
+            break;
+
+        }
+      
     }
+}
 
 
 int main()
 {
+    //Global variables================================
+    std::string name;
 
+    //current player funds and maximum player funds 
+    int playerFunds = 0;
+    int playerFundsMax = 0;
+
+    //money earn from work and stored in the bank
+    int bank = 150;
+
+    //money which can be earn from the casino
+    int casinoFunds = 0;
+    int choice;
+    //levels in the specific casino
+    CasinoBuilding currentCasino = CasinoBuilding::ROYAL;
+    CasinoBuilding previousCasino = CasinoBuilding::ROYAL;
+    CasinoRoom casinoRoom = CasinoRoom::ROOM_1;
 
     //================================================================
 
@@ -72,12 +106,14 @@ int main()
     std::cout << ". Your game will start now ! " << std::endl;
     std::cout << " Please choice number from the menu :" << std::endl;
 
+    //get choice from the menu
+    
 
     do
     {
-        int choice = Menu();
+        choice = Menu();
 
-        if (choice == 1)
+        if (choice == MenuCoice::NEXT_CASINO)
         {
            
             //randomly choosing if there is a free space in the casino
@@ -95,30 +131,11 @@ int main()
                 std::cout << " You are in the  " << std::endl;
                 // std::cout << " You are in : " << currentCasino << " casino " << std::endl;
 
-                switch (currentCasino)
-                {
-                    case CasinoBuilding::ROYAL:
-                    {
-                        std::cout << " Royal Building " << std::endl;
-                        break;
-                    }
-
-                    case CasinoBuilding::VEGAS:
-                    {
-                        std::cout << " Vegas Building " << std::endl;
-                        break;
-                    }
-
-                    case CasinoBuilding::MONACO:
-                    {
-                        std::cout << " Monaco Building " << std::endl;
-                        break;
-
-                    }
-                }
-
+                DisplayCasinoName(currentCasino);
+                break;
                 Casino();
                 choice = 0;
+               
                 
             }
             else
@@ -128,76 +145,39 @@ int main()
                 choice = 0;
             }
         }
-        else if (choice == 2)
+        else if (choice == MenuCoice::CURENT_CASINO)
         {
             std::cout << " Lets test your luck !!! " << std::endl;
             std::cout << " You remain in the " << std::endl;
             // std::cout << " You are in : " << currentCasino << " casino " << std::endl;
 
-            switch (currentCasino)
-            {
-                case CasinoBuilding::ROYAL:
-                {
-                    std::cout << " Royal Building " << std::endl;
-                    break;
-                }
-
-                case CasinoBuilding::VEGAS:
-                {
-                    std::cout << " Vegas Building " << std::endl;
-                    break;
-                }
-
-                case CasinoBuilding::MONACO:
-                {
-                    std::cout << " Monaco Building " << std::endl;
-                    break;
-
-                }
-            }
+            DisplayCasinoName(currentCasino);
+            break;
             Casino();
             choice = 0;
         }
-        else if (choice == 3)
+        else if (choice == MenuCoice::PREVIOUS_CASINO)
         {
             std::cout << " Lets test your luck !!! " << std::endl;
             std::cout << " You go back to  " << std::endl;
             // std::cout << " You are in : " << currentCasino << " casino " << std::endl;
 
-            switch (previousCasino)
-            {
-                case CasinoBuilding::ROYAL:
-                {
-                    std::cout << " Royal Building " << std::endl;
-                    break;
-                }
-
-                case CasinoBuilding::VEGAS:
-                {
-                    std::cout << " Vegas Building " << std::endl;
-                    break;
-                }
-
-                case CasinoBuilding::MONACO:
-                {
-                    std::cout << " Monaco Building " << std::endl;
-                    break;
-
-                }
+            DisplayCasinoName(currentCasino);
+            break;
                 Casino();
                 choice = 0;
                 currentCasino = previousCasino;
-            }
+            
         }
 
-        else if (choice == 4)
+        else if (choice == MenuCoice::EXIT)
         {
             std::cout << " Thank you for playing . Good buy ! " << std::endl;
         }
 
 
 
-    } while (choice != 4);
+    } while (choice != MenuCoice::EXIT);
 
     system("pause");
     return 0;
